32bf1b5224261fe1420266459a3cbcf7041fa2e7
Khanh Dang <khanh.dang@tekcent.com>
2018-08-13 14:59:10 +0700

    Update new FE source, add new navigation

--- src/assets/js/main-dist.js
+++ src/assets/js/main-dist.js
@@ -1,10 +1,10 @@
-!function(d, e, s, o) {
+!function(d, e, t, s) {
     "use strict";
-    var t = {
+    var i = {
         init: function() {
-            t.slideRender(), t.richTextInit(), t.navConfig(), d(e).scroll(function() {
-                t.navConfig();
-            }), d(".product-timer").length && setInterval(t.productTimerInit, 1e3);
+            i.slideRender(), i.richTextInit(), i.navConfig(), i.rateInit(), d(e).scroll(function() {
+                i.navConfig();
+            }), d(".product-timer").length && setInterval(i.productTimerInit, 1e3);
         },
         slideRender: function() {
             if (d(".banner-carousel").length && d(".banner-carousel").slick({
@@ -15,28 +15,28 @@
                 autoplaySpeed: 6e3,
                 arrows: !1,
                 dots: !0
-            }), d(".product-cols-carousel").length) for (var e = d(".product-cols-carousel"), s = 0; s < e.length; s++) {
-                var o = e.eq(s).data("items") || 1;
-                e.eq(s).slick({
+            }), d(".product-cols-carousel").length) for (var e = d(".product-cols-carousel"), t = 0; t < e.length; t++) {
+                var s = e.eq(t).data("items") || 1;
+                e.eq(t).slick({
                     infinite: !0,
-                    slidesToShow: o,
+                    slidesToShow: s,
                     slidesToScroll: 1,
                     autoplay: !0,
                     autoplaySpeed: 3e3,
                     responsive: [ {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: o - 1 < 1 ? 1 : o - 1
+                            slidesToShow: s - 1 < 1 ? 1 : s - 1
                         }
                     }, {
                         breakpoint: 768,
                         settings: {
-                            slidesToShow: o - 2 < 1 ? 1 : o - 2
+                            slidesToShow: s - 2 < 1 ? 1 : s - 2
                         }
                     }, {
                         breakpoint: 480,
                         settings: {
-                            slidesToShow: o - 3 < 1 ? 1 : o - 3
+                            slidesToShow: s - 3 < 1 ? 1 : s - 3
                         }
                     } ]
                 });
@@ -93,28 +93,41 @@
         },
         richTextInit: function() {
             if (d(".summernote").length) for (var e = 0; e < d(".summernote").length; e++) {
-                var s = d(".summernote").eq(e).data("placeholder") || "Vui lòng nhập", o = d(".summernote").eq(e).data("height") || 100;
+                var t = d(".summernote").eq(e).data("placeholder") || "Vui lòng nhập", s = d(".summernote").eq(e).data("height") || 100;
                 d(".summernote").eq(e).summernote({
-                    placeholder: s,
+                    placeholder: t,
                     tabsize: 2,
-                    height: o
+                    height: s
                 });
             }
         },
         productTimerInit: function() {
-            for (var e = d(".product-timer"), s = 0; s < e.length; s++) {
-                var o = e.eq(s).data("end"), t = 0, n = 0, i = 0;
-                if (null != o && 5 == o.split("-").length) {
-                    var a = o.split("-"), r = new Date(a[0], parseInt(a[1]) - 1, a[2], a[3], a[4]), l = new Date();
-                    l < r && (i = parseInt((r - l) / 1e3), n = parseInt(i / 60), t = parseInt(n / 60), 
-                    n %= 60, i %= 60);
+            for (var e = d(".product-timer"), t = 0; t < e.length; t++) {
+                var s = e.eq(t).data("end"), i = 0, a = 0, n = 0;
+                if (null != s && 5 == s.split("-").length) {
+                    var o = s.split("-"), r = new Date(o[0], parseInt(o[1]) - 1, o[2], o[3], o[4]), l = new Date();
+                    l < r && (n = parseInt((r - l) / 1e3), a = parseInt(n / 60), i = parseInt(a / 60), 
+                    a %= 60, n %= 60);
                 }
-                i = i < 10 ? "0" + i : i, n = n < 10 ? "0" + n : n, t = t < 10 ? "0" + t : t, e.eq(s).find("li").eq(0).text(t), 
-                e.eq(s).find("li").eq(1).text(n), e.eq(s).find("li").eq(2).text(i);
+                n = n < 10 ? "0" + n : n, a = a < 10 ? "0" + a : a, i = i < 10 ? "0" + i : i, e.eq(t).find("li").eq(0).text(i), 
+                e.eq(t).find("li").eq(1).text(a), e.eq(t).find("li").eq(2).text(n);
+            }
+        },
+        rateInit: function() {
+            if (d(".rate:not(.view)").length) {
+                var e = d(".rate:not(.view)");
+                e.find("a").click(function(e) {
+                    e.preventDefault(), d(this).parent().addClass("choiced"), d(this).parent().find("a").removeClass("active"), 
+                    d(this).prevAll().addClass("active"), d(this).addClass("active"), d(this).parent().parent().find(".rate-value").val(d(this).parent().find(".active").length), 
+                    d(this).parent().parent().find(".rate-text").text(d(this).parent().find(".active").length);
+                }), e.find("a").mouseenter(function() {
+                    d(this).parent().hasClass("choiced") || (d(this).parent().find("a").removeClass("active"), 
+                    d(this).prevAll().addClass("active"), d(this).addClass("active"), d(this).parent().parent().find(".rate-text").text(d(this).parent().find(".active").length));
+                });
             }
         }
     };
-    d(s).ready(function() {
-        t.init();
+    d(t).ready(function() {
+        i.init();
     });
 }(jQuery, window, document);
